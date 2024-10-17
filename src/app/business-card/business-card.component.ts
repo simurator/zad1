1168b0059162b2dc3a9b01a6440bab82d1f3cea3
Michael Wozniak <woobe-studio@proton.me>
2024-10-17 11:02:06 +0200

    fixed code and added cancel button

--- src/app/business-card/business-card.component.ts
+++ src/app/business-card/business-card.component.ts
@@ -7,27 +7,30 @@ import { ManageInterestsComponent } from '../manage-interests/manage-interests.c
 @Component({
   selector: 'app-business-card',
   standalone: true,
-  imports: [FormsModule,ShowDateComponent,CommonModule, ManageInterestsComponent],
+  imports: [FormsModule, ShowDateComponent, CommonModule, ManageInterestsComponent],
   templateUrl: './business-card.component.html',
   styleUrl: './business-card.component.css'
 })
 export class BusinessCardComponent {
-  name: string;
-  surname: string;
-  dateOfBirth: Date;
-  interests: string[];
-  selected:number=-1;
+  name: string = '';
+  surname: string = '';
+  dateOfBirth: Date = new Date();
+  interests: string[] = [];  // Initialize as an empty array
+  selected: number = -1;
 
-  constructor() { 
+  constructor() {
+    this.reset();
+  }
+
+  selectInterest(which: number): void {
+    this.selected = which;
+  }
+
+  reset(): void {
     this.name = 'Joanna';
     this.surname = 'Krupa';
     this.dateOfBirth = new Date('2000-03-18');
     this.interests = ['tennis', 'programming', 'photography'];
+    this.selected = -1;
   }
-
-  selectInterest(which:number):void{
-      this.selected=which;
-  }
-
-  
 }
